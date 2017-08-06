e07a57662cb846133669ef3cc4f78ba9e5684be4
Piotr <piotr.kontowicz@interia.pl>
2017-08-06 22:29:37 +0200

    ok

---
+++ BMI/BMI/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mass = new System.Windows.Forms.Label();
+            this.height = new System.Windows.Forms.Label();
+            this.mass_text_box = new System.Windows.Forms.TextBox();
+            this.height_taext_box = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mass
+            // 
+            this.mass.AutoSize = true;
+            this.mass.Location = new System.Drawing.Point(8, 72);
+            this.mass.Name = "mass";
+            this.mass.Size = new System.Drawing.Size(79, 17);
+            this.mass.TabIndex = 0;
+            this.mass.Text = "Masa ciała:";
+            // 
+            // height
+            // 
+            this.height.AutoSize = true;
+            this.height.Location = new System.Drawing.Point(16, 144);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(52, 17);
+            this.height.TabIndex = 1;
+            this.height.Text = "Wzrost";
+            // 
+            // mass_text_box
+            // 
+            this.mass_text_box.Location = new System.Drawing.Point(88, 72);
+            this.mass_text_box.Name = "mass_text_box";
+            this.mass_text_box.Size = new System.Drawing.Size(100, 22);
+            this.mass_text_box.TabIndex = 3;
+            // 
+            // height_taext_box
+            // 
+            this.height_taext_box.Location = new System.Drawing.Point(88, 144);
+            this.height_taext_box.Name = "height_taext_box";
+            this.height_taext_box.Size = new System.Drawing.Size(100, 22);
+            this.height_taext_box.TabIndex = 4;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(200, 64);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 152);
+            this.calculate.TabIndex = 6;
+            this.calculate.Text = "Oblicz";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.height_taext_box);
+            this.Controls.Add(this.mass_text_box);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.mass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mass;
+        private System.Windows.Forms.Label height;
+        private System.Windows.Forms.TextBox mass_text_box;
+        private System.Windows.Forms.TextBox height_taext_box;
+        private System.Windows.Forms.Button calculate;
+    }
+}
+
---
+++ BMI/BMI/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void message_box(string message, string title)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            result = MessageBox.Show(message, title, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+
+
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+                mass_text_box.Text = String.Empty;
+                height_taext_box.Text = String.Empty;
+            }
+
+        }
+
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double height = Double.Parse(height_taext_box.Text) / 100;
+                double mass = Double.Parse(mass_text_box.Text);
+
+                double bmi = ((mass) / (Math.Pow(height, 2)));
+
+                bmi = Math.Round(bmi, 2);
+                double correct_mass = 0;
+                string resutl = "Twoje BMI: " + bmi.ToString();
+                int p;
+                if (bmi < 16)
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nNiedowaga\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + "kg a ";
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if ((bmi >= 16) && (bmi <= 16.99))
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nWychudzenie\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + "kg a ";
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if ((bmi >= 17) && (bmi <= 18.49))
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nniedowaga\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + "kg a "; ;
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if ((bmi >= 18.5) && (bmi <= 24.99))
+                {
+                    resutl += "\nWaga prawidłowa";
+                }
+                if ((bmi >= 25) && (bmi <= 29.99))
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nNadwaga\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + "kg a "; ;
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if ((bmi >= 30) && (bmi <= 34.99))
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nI stopień otyłości\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + "kg a ";
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if ((bmi >= 35) && (bmi <= 39.99))
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nII stopień otyłości\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + " "; ;
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                if (bmi >= 40)
+                {
+                    correct_mass = 18.5 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += "\nostyłość skrajna\nPrawidłowa waga pomiędzy:" + correct_mass.ToString() + " "; ;
+                    correct_mass = 24.99 * Math.Pow(height, 2);
+                    correct_mass = Math.Floor(correct_mass);
+                    resutl += correct_mass.ToString() + "kg ";
+                }
+                resutl += "\nZamknąc program?";
+                message_box(resutl, "Result");
+            }
+            catch (Exception ex)
+            {
+                message_box("Something gone wrong\nDo You want close?", "Error");
+            }
+
+        }
+    }
+}
